6e2d1ab1b2245fef74b4525d53361cae592a43fe
Rimki <yhs693@naver.com>
2019-03-20 16:23:23 +0900

    main

--- app/src/main/java/com/example/kisa_market/MainActivity.java
+++ app/src/main/java/com/example/kisa_market/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.kisa_market;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import static java.security.AccessController.getContext;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  Button scanbutton;
+    private IntentIntegrator qrScan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanbutton =(Button)findViewById(R.id.scanButton);
+        qrScan =new IntentIntegrator(this);
+        scanbutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                qrScan.setPrompt("Scanning...");
+                qrScan.initiateScan();
+            }
+        });
     }
+
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //qrcode 가 없으면
+            if (result.getContents() == null) {
+           //     Toast.makeText(MainActivity.this, "취소!", Toast.LENGTH_SHORT).show();
+            } else {
+                //qrcode 결과가 있으면
+                Toast.makeText(MainActivity.this, "스캔완료!", Toast.LENGTH_SHORT).show();
+                try {
+                    //data를 json으로 변환
+                    JSONObject obj = new JSONObject(result.getContents());
+                    textViewName.setText(obj.getString("name"));
+                    textViewAddress.setText(obj.getString("address"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //Toast.makeText(MainActivity.this, result.getContents(), Toast.LENGTH_LONG).show();
+                    textViewResult.setText(result.getContents());
+                }
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }*/
 }
